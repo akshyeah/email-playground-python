6f56df5d174aae45a3bb57dbc210f25f2e76f311
Akshay kumar <akshay1.kumar@lntinfotech.com>
2021-07-07 16:16:04 +0530

    emailID removed for privacy

--- email_sender.py
+++ email_sender.py
@@ -1,16 +1,20 @@
+from os import name
 import smtplib
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path  #os path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
-email['from'] = 'Akshyeah'
-email['to'] = 'sender\'s email'
+email['from'] = 'Akshay kumar'
+email['to'] = 'recipient email-ID'
 email['subject'] = 'You won a million dollars'
 
-email.set_content('I am a python dev!')
+email.set_content(html.substitute({'name':'TInTin'}), 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('akshay19996@gmail.com', '96.attitude.19')
+    smtp.login('sender\s email-ID', 'sender\s password')
     smtp.send_message(email)
     print('Email sent successfully!!')
